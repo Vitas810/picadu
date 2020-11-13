bdd7b9dcd0688c86803e2e03e18f5409d8a57709
Vitas810 <59773453+Vitas810@users.noreply.github.com>
2020-11-13 19:51:58 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,25 +1,37 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBNyXR1jlTdSIHvqdeAjbll-0Ptie_PXFM',
+  authDomain: 'picadu-59e9f.firebaseapp.com',
+  databaseURL: 'https://picadu-59e9f.firebaseio.com',
+  projectId: 'picadu-59e9f',
+  storageBucket: 'picadu-59e9f.appspot.com',
+  messagingSenderId: '989629846678',
+  appId: '1:989629846678:web:4419ac71e42ac132ee5eb7',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+console.log(firebase);
+
 let menuTogle = document.querySelector('#menu-toggle');
 let menu = document.querySelector('.sidebar');
-
 const loginElem = document.querySelector('.login');
 const loginForm = document.querySelector('.login-form');
 const emailInput = document.querySelector('.login-email');
 const passwordInput = document.querySelector('.login-password');
 const loginSignup = document.querySelector('.login-signup');
-
 const userElem = document.querySelector('.user');
 const userNameElem = document.querySelector('.user-name');
-
 const exitElem = document.querySelector('.exit');
 const editElem = document.querySelector('.edit');
 const editContainer = document.querySelector('.edit-container');
-
 const editUsername = document.querySelector('.edit-username');
 const editPhotourl = document.querySelector('.edit-photo');
-
 const userAvatarElem = document.querySelector('.user-avatar');
-
 const postWraper = document.querySelector('.posts');
+
+const buttonNewPost = document.querySelector('.button-new-post');
+const addPostElem = document.querySelector('.add-post');
 
 const checkEmail = () => {
   const re = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
@@ -34,6 +46,8 @@ const listUsers = [
     email: 'maks@mail.com',
     password: '12345',
     displayName: 'maxJS',
+    photo:
+      'https://avatars.mds.yandex.net/get-zen_doc/28532/pub_5d1eb72a8e297300ad9a2fce_5d1ebcf1af329300adf417c6/scale_2400',
   },
   {
     id: '02',
@@ -62,16 +76,12 @@ const setUsers = {
     let resValue = checkEmail();
 
     if (resValue) {
-      console.log('val');
+      /*       console.log('valid'); */
     } else {
       alert('email  не валидный, попробуйте ввести заново');
       return;
     }
 
-    /*  if (!email.trim() || !password.trim()) {
-      alert('введите данные');
-      return;
-    } */
     to = email.indexOf('@');
 
     if (!this.getUser(email)) {
@@ -108,11 +118,15 @@ const setUsers = {
 const setPost = {
   allPost: [
     {
-      title: 'Заголоыок поста',
+      title: 'Заголовок поста',
       text:
         'Далеко-далеко за, словесными горами в стране гласных и согласных живут рыбные тексты. Ты единственное его раз великий ему! Однажды семантика то',
       tags: ['свежее', 'новое', 'горячее', 'мое', 'случайное'],
-      author: 'maks@mail.com',
+      author: {
+        displayName: 'maks',
+        photo:
+          'https://avatars.mds.yandex.net/get-zen_doc/28532/pub_5d1eb72a8e297300ad9a2fce_5d1ebcf1af329300adf417c6/scale_2400',
+      },
       date: '11.11.2020, 20:54:00',
       like: 15,
       comments: 20,
@@ -122,12 +136,34 @@ const setPost = {
       text:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus at doloribus eos, blanditiis eligendi voluptas! Aspernatur atque numquam quos, animi dolor obcaecati saepe placeat commodi dicta quibusdam soluta voluptatum blanditiis.',
       tags: ['свежее', 'новое', 'горячее', 'мое', 'случайное'],
-      author: 'test@mail.com',
+      author: {
+        displayName: 'kate',
+        photo:
+          'https://i.pinimg.com/originals/f0/a6/4e/f0a64e32194d341befecc80458707565.jpg',
+      },
       date: '10.10.2020, 20:54:00',
       like: 5,
       comments: 30,
     },
   ],
+  addPost(title, text, tags, handler) {
+    this.allPost.unshift({
+      title,
+      text,
+      tags: tags.split(',').map((item) => item.trim()),
+      author: {
+        displayName: setUsers.user.displayName,
+        photo: setUsers.user.photo,
+      },
+      date: new Date().toLocaleString(),
+      like: 0,
+      comments: 0,
+    });
+
+    if (handler) {
+      handler();
+    }
+  },
 };
 
 const toggleAuthDom = () => {
@@ -138,10 +174,19 @@ const toggleAuthDom = () => {
     userElem.style.display = '';
     userNameElem.textContent = user.displayName;
     userAvatarElem.src = user.photo || userAvatarElem.src;
+    buttonNewPost.classList.add('visible');
   } else {
     loginElem.style.display = '';
     userElem.style.display = 'none';
+    buttonNewPost.classList.remove('visible');
+    addPostElem.classList.remove('visible');
+    postWraper.classList.add('visible');
   }
+};
+
+const showAddPost = () => {
+  addPostElem.classList.add('visible');
+  postWraper.classList.remove('visible');
 };
 
 loginForm.addEventListener('submit', (event) => {
@@ -151,9 +196,12 @@ loginForm.addEventListener('submit', (event) => {
 });
 
 const showAllPosts = () => {
+  addPostElem.classList.remove('visible');
+  postWraper.classList.add('visible');
+
   let postsHTML = '';
   setPost.allPost.forEach((post) => {
-    const { title, text, date, like, comments, tags } = post;
+    const { title, text, date, like, comments, tags, author } = post;
 
     postsHTML += `
           <section class="post">
@@ -162,11 +210,9 @@ const showAllPosts = () => {
               <p class="post-text">${text}</p>
               </p>
               <div class="tags">
-              ${tags
-                .map((item) => {
-                  return ` <a href="#" class="tag">#${item}</a>`;
-                })
-                .join('')}
+              ${tags.map((item) => {
+                return ` <a href="#" class="tag">#${item}</a>`;
+              })}
                 
               </div>
               <!-- /.tags -->
@@ -200,11 +246,13 @@ const showAllPosts = () => {
               <!-- /.post-buttons -->
               <div class="post-author">
                 <div class="author-about">
-                  <a href="" class="author-username">artem</a>
+                  <a href="" class="author-username">${author.displayName}</a>
                   <span class="post-time"> ${date}</span>
                 </div>
                 <a href="#" class="author-link"
-                  ><img src="img/avatar.jpg" alt="avatar" class="author-avatar"
+                  ><img src="${
+                    author.photo || 'img/avatar.jpg'
+                  }" alt="avatar" class="author-avatar"
                 /></a>
               </div>
               <!-- /.post-author -->
@@ -246,6 +294,31 @@ const init = () => {
   menuTogle.addEventListener('click', (el) => {
     el.preventDefault();
     menu.classList.toggle('visible');
+  });
+
+  buttonNewPost.addEventListener('click', (event) => {
+    event.preventDefault();
+    showAddPost();
+  });
+
+  addPostElem.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const { title, text, tags } = addPostElem.elements;
+
+    if (title.value.length < 6) {
+      alert('короткий заголовок');
+      return;
+    }
+
+    if (text.value.length < 50) {
+      alert('короткий текст');
+      return;
+    }
+
+    setPost.addPost(title.value, text.value, tags.value, showAllPosts);
+
+    addPostElem.classList.remove('visible');
+    addPostElem.reset();
   });
 
   showAllPosts();
